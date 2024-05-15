be43dc1c749f15f02aa599f82c42e12f10601fee
huerta2502 <huerta2502@gmail.com>
2024-05-15 17:24:20 -0600

    Carousel item for experience section modeled

---
+++ src/interfaces/ExperienceItem.ts
@@ -0,0 +1,10 @@
+export default interface ExperienceItem {
+  index: number;
+  cover: string;
+  coverSource: string;
+  title: string;
+  startYear: number;
+  endYear: number;
+  description: string;
+  href: string;
+}
