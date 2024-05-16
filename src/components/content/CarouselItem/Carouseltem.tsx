b8293c8448c1e970f650f17bc5196e1296b08ed1
huerta2502 <huerta2502@gmail.com>
2024-05-16 12:48:48 -0600

    Carousel items content dialog added

--- src/components/content/CarouselItem/Carouseltem.tsx
+++ src/components/content/CarouselItem/Carouseltem.tsx
@@ -3,35 +3,83 @@ import "./CarouselItem.css";
 
 const CarouselItem = ({ experience }: { experience: ExperienceItem }) => {
   return (
-    <article className='no-padding round tertiary-container'>
-      {experience.cover === "video" ? (
-        <video className='carousel-item responsive' muted loop playsInline>
-          <source src={experience.coverSource} type='video/mp4' />
-        </video>
-      ) : (
+    <>
+      <article className='no-padding round tertiary-container'>
         <img
           className='carousel-item responsive'
           src={experience.coverSource}
           loading='lazy'
           alt={experience.title.concat(" cover")}
         />
-      )}
-      <div className='absolute bottom left right padding bottom-shadow bottom-round white-text'>
-        <h5>{experience.title}</h5>
-        <div className='max'></div>
+        <div className='absolute bottom left right padding bottom-shadow bottom-round white-text'>
+          <h5>{experience.title}</h5>
+          <div className='max'></div>
+          <nav>
+            <span className='large-text max'>
+              {experience.startYear}
+              {experience.startYear != experience.endYear && (
+                <span> - {experience.endYear}</span>
+              )}
+            </span>
+            <button
+              className='circle transparent'
+              data-ui={"#xp".concat(String(experience.index))}
+            >
+              <i>expand_content</i>
+            </button>
+          </nav>
+        </div>
+      </article>
+      <dialog className='max fill' id={"xp".concat(String(experience.index))}>
         <nav>
-          <span className='large-text max'>
-            {experience.startYear}
-            {experience.startYear != experience.endYear && (
-              <span> - {experience.endYear}</span>
-            )}
-          </span>
-          <button className='circle transparent'>
-            <i>expand_content</i>
+          <div className='max'>
+            <h5 className='wrap'>{experience.title}</h5>
+          </div>
+          <button
+            className='circle transparent'
+            data-ui={"#xp".concat(String(experience.index))}
+          >
+            <i>collapse_content</i>
           </button>
         </nav>
-      </div>
-    </article>
+        {experience.media ? (
+          <iframe
+            loading='lazy'
+            style={{ height: "50vh" }}
+            className='responsive no-border top-margin round'
+            src={experience.media}
+            title={experience.title}
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+            referrerPolicy='strict-origin-when-cross-origin'
+            allowFullScreen
+          ></iframe>
+        ) : (
+          <img
+            className='responsive large-height top-margin round'
+            src={experience.coverSource}
+            loading='lazy'
+            alt={experience.title.concat(" cover")}
+          />
+        )}
+        <p className='bottom-padding'>{experience.description}</p>
+        <nav className='right-align'>
+          <button
+            className='border'
+            data-ui={"#xp".concat(String(experience.index))}
+          >
+            Close
+          </button>
+          <a
+            className='button'
+            href={experience.href}
+            target='_blank'
+            rel='noreferrer'
+          >
+            Visit
+          </a>
+        </nav>
+      </dialog>
+    </>
   );
 };
 
--- src/interfaces/ExperienceItem.ts
+++ src/interfaces/ExperienceItem.ts
@@ -1,10 +1,10 @@
 export default interface ExperienceItem {
   index: number;
-  cover: string;
   coverSource: string;
   title: string;
   startYear: number;
   endYear: number;
   description: string;
   href: string;
+  media?: string;
 }
